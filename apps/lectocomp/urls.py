bfc50842f128d608f88e129ba391b44b308ec87a
edgardo javier <edgardojavierchaco@gmail.com>
2024-06-12 00:19:44 -0300

    dashboard evaluacion

--- apps/lectocomp/urls.py
+++ apps/lectocomp/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import tu_vista
+from .views import tu_vista, cargar_grafico_reg, mostrar_grafico_reg
 
 app_name='lectocomprension'
 
 urlpatterns = [    
-    path('resultados/', tu_vista, name='resultados'),    
+    path('resultados/', tu_vista, name='resultados'),   
+    path('cargar_graficoreg/', cargar_grafico_reg, name='cargar_graficoreg'),
+    path('grafico_reg/', mostrar_grafico_reg, name='grafico_reg'), 
 ]
--- apps/lectocomp/views.py
+++ apps/lectocomp/views.py
@@ -1,7 +1,10 @@
+import psycopg2
 import plotly.graph_objs as go
 from django.db import connection
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+import pandas as pd
+from django.http import HttpResponse
 
 def calcular_estadisticas_por_cueanexo(username):
     with connection.cursor() as cursor:
@@ -56,4 +59,86 @@ def tu_vista(request):
         'total_dni_sin_desempenio': total_dni_sin_desempenio,
         'promedio_puntaje': promedio_puntaje,
         'grafico_torta_total_dni': grafico_torta_total_dni.to_html(full_html=False),
+    })
+
+
+def mostrar_grafico_reg(request):
+    # Obtener los valores seleccionados del checkbox
+    regiones_seleccionadas = request.GET.getlist('region')
+    
+    mostrar_todo = '0' in regiones_seleccionadas  # Verifica si se selecciona "Mostrar Todo"
+    
+    # Establecer conexión con la base de datos PostgreSQL
+    with psycopg2.connect(
+        dbname="visualizador",
+        user="visualizador",
+        password="Estadisticas24",
+        host="relevamientoanual.com.ar"
+    ) as conn:
+        with conn.cursor() as cursor:
+            if mostrar_todo:
+                # Si se selecciona "Mostrar Todo", mostrar todos los datos
+                query = "SELECT desempenio, puntaje, reg FROM public.teslecturamayo"
+                cursor.execute(query)
+            else:
+                # Filtrar los datos para las regiones seleccionadas
+                if regiones_seleccionadas:
+                    placeholders = ','.join(['%s'] * len(regiones_seleccionadas))
+                    query = f"SELECT desempenio, puntaje, reg FROM public.teslecturamayo WHERE reg IN ({placeholders})"
+                    cursor.execute(query, tuple(regiones_seleccionadas))
+                else:
+                    return render(request, 'lectocomp/graficoreg.html', {
+                        'datos_disponibles': False
+                    })
+            
+            # Obtener los datos de la consulta
+            datos_usuario = cursor.fetchall()
+    
+    if not datos_usuario:
+        return render(request, 'lectocomp/graficoreg.html', {
+            'datos_disponibles': False
+        })
+    
+    # Convertir los datos a un DataFrame de pandas
+    df = pd.DataFrame(datos_usuario, columns=['desempenio', 'puntaje', 'reg'])
+
+    # Calcular el total de alumnos evaluados
+    total_alumnos = df.shape[0]
+    
+    # Verificar si las columnas 'desempenio' y 'puntaje' existen
+    if 'desempenio' not in df.columns or 'puntaje' not in df.columns:
+        return HttpResponse("Error en los datos: columnas 'desempenio' o 'puntaje' no encontradas.")
+    
+    # Agrupar y contar por categoría de desempeño
+    conteo_desempenio = df['desempenio'].value_counts()
+    
+    # Calcular los porcentajes
+    porcentajes = (conteo_desempenio / conteo_desempenio.sum()) * 100
+    
+    # Calcular el promedio de puntaje
+    promedio_puntaje = round(df['puntaje'].mean(), 2)
+    
+    # Obtener el nombre de la región o mostrar "Chaco"
+    regional = 'Chaco' if mostrar_todo else ', '.join(df['reg'].unique())
+    
+    # Crear el gráfico de torta con Plotly
+    fig = go.Figure(data=[go.Pie(labels=porcentajes.index, values=porcentajes)])
+    fig.update_layout(
+                      showlegend=True)
+    
+    # Convertir la figura a HTML
+    graph_html = fig.to_html(full_html=False, default_height=500, default_width=700)
+    
+    # Renderizar la plantilla y pasar el gráfico y el promedio de puntaje
+    return render(request, 'lectocomp/graficoreg.html', {
+        'grafico': graph_html,
+        'promedio_puntaje': promedio_puntaje,
+        'total_alumnos': total_alumnos,
+        'regional': regional,
+        'datos_disponibles': True
+    })
+
+def cargar_grafico_reg(request):    
+    return render(request, 'lectocomp/graficoreg.html', {
+        'datos_disponibles': False
     })
