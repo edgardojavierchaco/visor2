f6dead4685614c25ba2a630653b78100f78b8473
edgardo javier <edgardojavierchaco@gmail.com>
2024-08-30 09:24:15 -0300

    agregado opciones fluidez por localidad

--- apps/lectocomp/urls.py
+++ apps/lectocomp/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
 from .views import tu_vista, cargar_grafico_reg, mostrar_grafico_reg, mostrar_pdf_recomendaciones, mostrar_pdf_informefinal
+from .views import mostrar_grafico_localidad, cargar_grafico_loc, mostrar_grafico_loc
 
-app_name='lectocomprension'
+app_name = 'lectocomprension'
 
 urlpatterns = [    
     path('resultados/', tu_vista, name='resultados'),   
     path('cargar_graficoreg/', cargar_grafico_reg, name='cargar_graficoreg'),
+    path('cargar_graficoloc/', mostrar_grafico_localidad, name='cargar_graficoloc'),
     path('grafico_reg/', mostrar_grafico_reg, name='grafico_reg'),
+    path('grafico_loc/', mostrar_grafico_loc, name='grafico_loc'),
     path('recomendaciones/', mostrar_pdf_recomendaciones, name='recomendaciones'),
-    path('informe/', mostrar_pdf_informefinal, name='informe'),
+    path('informe/', mostrar_pdf_informefinal, name='informe'),       
 ]
--- apps/lectocomp/views.py
+++ apps/lectocomp/views.py
@@ -189,3 +189,126 @@ def mostrar_pdf_recomendaciones(request):
 def mostrar_pdf_informefinal(request):
     pdf_url = "https://drive.google.com/file/d/1KIRcY6BfwwR9oyttD3xYqiixC_2akBfr/preview"
     return render(request, 'lectocomp/informefinal.html',{'pdf_url': pdf_url})
+
+
+#####################################################################
+#                 MUESTRA GRAFICO FILTRADO POR LOCALIDAD            #
+#####################################################################
+
+def mostrar_grafico_localidad(request):
+    localidades=[]       
+    
+    # Obtine las localidades únicas
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT DISTINCT localidad FROM public.v_teslecturamayoeval WHERE localidad !='None'")
+        localidades = [row[0] for row in cursor.fetchall()]
+        print('Localidades cargadas:', localidades)
+    
+     # Si es una solicitud estándar, renderiza la plantilla
+    context = {
+        'localidades': localidades,        
+    }
+    print('Contexto:', context)
+    return render(request, 'lectocomp/graficolocalidades.html', context)
+
+
+def mostrar_grafico_loc(request):
+    # Obtiene las localidades únicas para cargar en el select
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT DISTINCT localidad FROM public.v_teslecturamayoeval WHERE localidad !='None'")
+        localidades = [row[0] for row in cursor.fetchall()]
+    
+    # Obtiene los valores seleccionados en el select    
+    localidades_seleccionadas = request.GET.get('localidad')   
+    
+    print('localidad_seleccionada:',localidades_seleccionadas)    
+    
+    # Establece conexión con la base de datos PostgreSQL
+    with psycopg2.connect(
+        dbname="visualizador",
+        user="visualizador",
+        password="Estadisticas24",
+        host="visoreducativochaco.com.ar"
+    ) as conn:
+        with conn.cursor() as cursor:            
+                # Filtrar los datos para las regiones seleccionadas
+                if localidades_seleccionadas:                    
+                    query = f"SELECT desempenio, puntaje, localidad FROM public.v_teslecturamayoeval WHERE localidad = '{localidades_seleccionadas}'"
+                    cursor.execute(query, localidades_seleccionadas)
+                    datos_usuario = cursor.fetchall()
+                    print('datos de usuario:', datos_usuario)
+                    query1 = f"SELECT desempenio3, puntaje, localidad FROM public.v_teslecturamayoeval WHERE localidad = '{localidades_seleccionadas}'"
+                    cursor.execute(query1, localidades_seleccionadas)
+                    datos_usuario1 = cursor.fetchall()
+                else:
+                    return render(request, 'lectocomp/graficolocalidades.html', {
+                        'datos_disponibles': False
+                    })
+    
+    if not datos_usuario or not datos_usuario1:
+        return render(request, 'lectocomp/graficolocalidades.html', {
+            'datos_disponibles': False
+        })
+    
+    # Convertir los datos a DataFrames de pandas
+    df = pd.DataFrame(datos_usuario, columns=['desempenio', 'puntaje', 'localidad'])
+    df1 = pd.DataFrame(datos_usuario1, columns=['desempenio3', 'puntaje', 'localidad'])
+
+    # Calcular el total de alumnos evaluados
+    total_alumnos = df.shape[0]
+    
+    # Verificar si las columnas 'desempenio' y 'puntaje' existen
+    if 'desempenio' not in df.columns or 'puntaje' not in df.columns:
+        return HttpResponse("Error en los datos: columnas 'desempenio' o 'puntaje' no encontradas.")
+    
+    if 'desempenio3' not in df1.columns or 'puntaje' not in df1.columns:
+        return HttpResponse("Error en los datos: columnas 'desempenio3' o 'puntaje' no encontradas.")
+    
+    # Definir el orden y colores para los labels
+    orden_labels = ["Debajo del Nivel Básico", "Básico", "Satisfactorio", "Avanzado", "Calificación Incorrecta", "Sin Calificar"]
+    colores = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b']
+    
+    # Ajustar los conteos según el orden definido
+    conteo_desempenio = df['desempenio'].value_counts().reindex(orden_labels, fill_value=0)
+    conteo_desempenio3 = df1['desempenio3'].value_counts().reindex(orden_labels, fill_value=0)
+    
+    # Calcular los porcentajes
+    porcentajes = (conteo_desempenio / conteo_desempenio.sum()) * 100
+    porcentajes3 = (conteo_desempenio3 / conteo_desempenio3.sum()) * 100
+    
+    # Crear etiquetas con cantidad y porcentaje
+    etiquetas = [f"{label}: {conteo} ({porcentaje:.2f}%)" for label, conteo, porcentaje in zip(orden_labels, conteo_desempenio, porcentajes)]
+    etiquetas3 = [f"{label}: {conteo} ({porcentaje:.2f}%)" for label, conteo, porcentaje in zip(orden_labels, conteo_desempenio3, porcentajes3)]
+    
+    # Calcular el promedio de puntaje
+    promedio_puntaje = round(df['puntaje'].mean(), 2)
+    
+    # Obtener el nombre de la región o mostrar "Chaco"
+    localidad = localidades_seleccionadas
+    
+    # Crear los gráficos de torta con Plotly
+    fig = go.Figure(data=[go.Pie(labels=etiquetas, values=conteo_desempenio, marker=dict(colors=colores))])
+    fig.update_layout(showlegend=True)
+    
+    fig3 = go.Figure(data=[go.Pie(labels=etiquetas3, values=conteo_desempenio3, marker=dict(colors=colores))])
+    fig3.update_layout(showlegend=True)
+    
+    # Convertir las figuras a HTML
+    graph_html = fig.to_html(full_html=False, default_height=500, default_width=700)
+    graph_html3 = fig3.to_html(full_html=False, default_height=500, default_width=700)
+    
+    # Renderizar la plantilla y pasar ambos gráficos y el promedio de puntaje
+    return render(request, 'lectocomp/graficolocalidades.html', {
+        'grafico': graph_html,
+        'grafico3': graph_html3,
+        'promedio_puntaje': promedio_puntaje,
+        'total_alumnos': total_alumnos,
+        'localidades': localidades,
+        'localidad': localidades_seleccionadas,
+        'datos_disponibles': True
+    })
+
+def cargar_grafico_loc(request):    
+    return render(request, 'lectocomp/graficolocalidades.html', {
+        'datos_disponibles': False
+    })
