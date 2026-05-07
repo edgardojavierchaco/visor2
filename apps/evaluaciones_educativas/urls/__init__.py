f364d8e228930dce1fe9a7b3181438d41390ee66
edgardojavierchaco@gmail.com <edgardojavierchaco@gmail.com>
2026-05-07 12:45:56 -0300

    correcciones en urls de evaluación educativa

--- apps/evaluaciones_educativas/urls/__init__.py
+++ apps/evaluaciones_educativas/urls/__init__.py
@@ -4,6 +4,6 @@ from django.urls import path, include
 app_name='evaluaciones_educativas'
 
 urlpatterns = [
-    path('fluidez_2025/', include('apps.evaluaciones_educativas.urls.fluidez_2025')),
-    path('diagnostico_2026/', include('apps.evaluaciones_educativas.urls.diagnostico_2026')),
+    path('fluidez_2025/', include('apps.evaluaciones_educativas.urls.fluidez_2025', namespace='fluidez_2025')),
+    path('diagnostico_2026/', include('apps.evaluaciones_educativas.urls.diagnostico_2026', namespace='diagnostico_2026')),
 ]
