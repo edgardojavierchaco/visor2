0f7af29861272e3e24ae1618ffa7aa3149745af0
Santini Martin <Martin_Santini219@outlook.es>
2026-06-18 12:54:54 -0300

    Se añade fluidez 2026

--- apps/evaluaciones_educativas/urls/__init__.py
+++ apps/evaluaciones_educativas/urls/__init__.py
@@ -6,4 +6,5 @@ app_name='evaluaciones_educativas'
 urlpatterns = [
     path('fluidez_2025/', include('apps.evaluaciones_educativas.urls.fluidez_2025', namespace='fluidez_2025')),
     path('diagnostico_2026/', include('apps.evaluaciones_educativas.urls.diagnostico_2026', namespace='diagnostico_2026')),
+    path('fluidez_2026/', include('apps.evaluaciones_educativas.urls.fluidez_2026', namespace='fluidez_2026')),
 ]
