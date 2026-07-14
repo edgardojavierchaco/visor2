b70410d6490ff0d342c3d04c6c61cc9dfd5f2c88
Elias <eliasmorales.21@hotmail.com>
2026-07-14 16:55:27 -0300

    Solucion para carga de fluidez 2026 (URL) y se añade visualizacion fluidez 2026 para funcionarios

--- apps/evaluaciones_educativas/urls/fluidez_2026.py
+++ apps/evaluaciones_educativas/urls/fluidez_2026.py
@@ -14,9 +14,12 @@ urlpatterns = [
     path('lista_examen',fluidez_2026.lista_examen, name='lista_examen'),
     path('lista_examen/<str:fid_actual>/',fluidez_2026.lista_examen, name='lista_examen'),
     path('actualizar_seccion/<uuid:alumno_public_id>/', fluidez_2026.actualizar_seccion, name='actualizar_seccion'),
-    # path('analisis_evaluacion/',fluidez_2026.analisis_evaluaciones_junio_2026, name='analisis_evaluacion'),
-    # path('analisis_evaluacion_junio_2026/',fluidez_2026.analisis_evaluaciones_regional_junio_2026, name='analisis_evaluacion_junio_2026'),
-    # path('analisis_completo_evaluacion_junio_2026/',fluidez_2026.analisis_evaluaciones_ministros_junio_2026, name='analisis_completo_evaluacion_junio_2026'),
+    path('borrar_registro_alumno/<uuid:alumno_public_id>/<str:fid_actual>/',fluidez_2026.borrar_registro_alumno, name='borrar_registro_alumno'),
+	path('descargar_excel/<uuid:grado_public_id>/',fluidez_2026.descargar_excel, name='excel'),
+    path('completar_carga/<uuid:grado_public_id>/',fluidez_2026.completar_carga, name='completar_carga'),
+    #path('analisis_evaluacion/',fluidez_2026.analisis_evaluaciones_junio_2026, name='analisis_evaluacion'),
+    #path('analisis_evaluacion_junio_2026/',fluidez_2026.analisis_evaluaciones_regional_junio_2026, name='analisis_evaluacion_junio_2026'),
+    path('analisis_completo_evaluacion_junio_2026/',fluidez_2026.analisis_evaluaciones_ministros_junio_2026, name='analisis_completo_evaluacion_junio_2026'),
     path('<str:fid_actual>/',fluidez_2026.lista, name='lista'),
 	
 ]
